afa7727865f25194bb5b14cd198ad59413cf4dbc
Arthur Faria <arthurgomesfaria@gmail.com>
2017-10-26 02:56:37 -0200

    Translation

--- src/util/exceptionMessages.js
+++ src/util/exceptionMessages.js
@@ -1,11 +1,11 @@
 const ExceptionMessages = {
   TOO_MANY_QUADRANTS: 'Existem mais de 4 quadrantes listados. Verifique se há apenas 4 tipos de quadrante na coluna.',
   TOO_MANY_RINGS: 'Há mais de 4 rings.',
-  MISSING_HEADERS: 'Document is missing one or more required headers or they are misspelled. ' +
-  'Check that your document contains headers for "name", "ring", "quadrant", "isNew", "description".',
-  MISSING_CONTENT: 'Document is missing content.',
-  LESS_THAN_FOUR_QUADRANTS : 'There are less than 4 quadrant names listed in your data. Check the quadrant column for errors.',
-  SHEET_NOT_FOUND: 'Oops! We can’t find the Google Sheet you’ve entered. Can you check the URL?'
+  MISSING_HEADERS: 'O documento não foi encontrado ou as colunas tem nomes diferentes. ' +
+  'Verifique se o documento possui as colunas nomeadas na seguinte ordem:"name", "ring", "quadrant", "maturity", "visible", "description".',
+  MISSING_CONTENT: 'O documento está vazio.',
+  LESS_THAN_FOUR_QUADRANTS : 'Há menos de 4 quadrantes no seu documento. Verifique a coluna "quadrant" por possíveis erros.',
+  SHEET_NOT_FOUND: 'Oops! Não foi possível encontrar a planilha. Você pode verificar se ela está publicada devidamente e a URL está correta?'
 };
 
 module.exports = ExceptionMessages;
